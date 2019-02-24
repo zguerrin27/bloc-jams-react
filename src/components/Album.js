831e5b73d611a9f11dac04fb3e53e6f42a772a7f
Zachary Guerrin <39317224+zguerrin27@users.noreply.github.com>
2019-02-24 14:34:43 -0500

    Assignment-6: Completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -32,6 +32,15 @@ class Album extends Component {
              <col id="song-duration-column" />
            </colgroup>  
            <tbody>
+              {
+                this.state.album.songs.map( (song, index) =>
+                <tr className="song" key={index}>
+                    <td className="song-number">{index + 1}</td>
+                    <td className="song-title">{song.title}</td>
+                    <td className="song-duration">{song.duration}</td>
+                </tr>
+                )
+              }
            </tbody>
          </table>
         </section>
